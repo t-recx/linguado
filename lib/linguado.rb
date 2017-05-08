7323eee9f1022424bb908c2e3bc2266dfdc19cba
João Bruno <bruno.joao@live.com.pt>
2017-05-08 22:24:41 +0100

    Added WordPolicy class

--- lib/linguado.rb
+++ lib/linguado.rb
@@ -1,6 +1,7 @@
 require "linguado/version"
 require "linguado/lesson"
 require "linguado/speaker"
+require "linguado/word_policy"
 
 module Linguado
   # Your code goes here...
---
+++ lib/linguado/word_policy.rb
@@ -0,0 +1,41 @@
+require 'text'
+
+class WordPolicy
+  include Text
+
+  attr_accessor :condition
+  attr_accessor :levenshtein_distance_allowed
+  attr_accessor :exceptions
+
+  def initialize condition: lambda { |word| true }, levenshtein_distance_allowed: 0, exceptions: []
+    @condition = condition
+    @levenshtein_distance_allowed = 0
+    @exceptions = exceptions
+  end
+
+  def passes? word, original_word
+    word = word.downcase
+    original_word = original_word.downcase
+
+    return true if word == original_word
+
+    return true unless condition.call original_word
+
+    return false if exceptions.include? word
+
+    return true if typo? word, original_word
+
+    return false
+  end
+
+  def typo? word, original_word
+    word = word.downcase
+    original_word = original_word.downcase
+
+    return false if word == original_word
+
+    return true if Levenshtein.distance(word, original_word) <= levenshtein_distance_allowed
+
+    return false
+  end
+end
